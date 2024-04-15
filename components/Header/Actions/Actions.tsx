c6e28e2f4a877f69e6e80b963028b14a2203ab45
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-15 17:25:49 +0100

    feat: Add Avatar component to Actions.tsx

--- components/Header/Actions/Actions.tsx
+++ components/Header/Actions/Actions.tsx
@@ -1,12 +1,16 @@
 import { Bell, Heart } from 'lucide-react';
 import NavButton from '../NavButton/NavButton';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const Actions = () => {
   return (
     <div className='flex gap-x-4'>
       <NavButton icon={Heart} />
       <NavButton icon={Bell} />
-      <div>User Avatar</div>
+      <Avatar>
+        <AvatarImage src='' />
+        <AvatarFallback className='bg-sky-500/50'>MH</AvatarFallback>
+      </Avatar>
     </div>
   );
 };
