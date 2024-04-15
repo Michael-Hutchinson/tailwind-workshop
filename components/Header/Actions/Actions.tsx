203ef7ab1ed47726e55bd1d02de0fb2388f81a38
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-15 16:52:07 +0100

    feat: Add Actions component to Header

---
+++ components/Header/Actions/Actions.tsx
@@ -0,0 +1,13 @@
+import { Bell, Heart } from 'lucide-react';
+
+const Actions = () => {
+  return (
+    <div className='flex gap-x-4'>
+      <Heart />
+      <Bell />
+      <div>User Avatar</div>
+    </div>
+  );
+};
+
+export default Actions;
