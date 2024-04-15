2e61efa5e37b7a4f465783d1fe71f443198369ee
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-15 17:45:30 +0100

    feat: Update max-width in Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Navigation from './Navigation/Navigation';
 const Header = () => {
   return (
     <header className='h-16 border-b'>
-      <nav className='flex justify-between max-w-screen-md mx-auto h-full items-center px-6'>
+      <nav className='flex justify-between max-w-screen-xl mx-auto h-full items-center px-6'>
         <Logo />
         <Navigation />
         <Actions />
