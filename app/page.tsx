df02c4e880d2276067ed93273128659999633239
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-16 17:12:22 +0100

    feat: Add Filters and Results components, and display map image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Filters from '@/components/Filters/Filters';
+import Results from '@/components/Results/Results';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className='h-[calc(100%-4rem)] grid grid-cols-5 p-6 max-w-screen-xl mx-auto gap-4'>
+      <div className='h-full col-span-3 space-y-4'>
+        <Filters />
+        <Results />
+      </div>
+      <div className='h-full col-span-2 rounded-xl bg-blue-400 relative overflow-hidden'>
+        <Image fill alt='' src='/map.jpg' className='object-cover' />
+      </div>
     </div>
   );
 }
