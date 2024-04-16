8bea9e9fc3ef95f07999532b2a27d926eadd33e2
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-16 08:59:41 +0100

    feat: Add filter input and search button to Filters component

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,8 +1,10 @@
+import { Button } from '../ui/button';
 import FilterButton from './FilterButton/FilterButton';
+import FilterInput from './FilterInput/FilterInput';
 
 const Filters = () => {
   return (
-    <div className='bg-[#F4F6F9] rounded-xl p-6'>
+    <div className='bg-[#F4F6F9] rounded-xl p-6 space-y-4'>
       <div className='flex items-center justify-between'>
         <div className='flex items-center gap-x-2'>
           <FilterButton label='Hotel' active />
@@ -10,6 +12,24 @@ const Filters = () => {
           <FilterButton label='Village' />
         </div>
         <p className='text-muted-foreground text-sm'>Today: Apr 16, Tue</p>
+      </div>
+      <div className='flex drop-shadow-2xl items-end w-full'>
+        <FilterInput
+          label='City or hotel'
+          placeholder='Search'
+          className='rounded-l-xl rounded-r-none'
+        />
+        <FilterInput
+          label='Check-in'
+          placeholder='Date'
+          className='rounded-l-none rounded-r-none'
+        />
+        <FilterInput
+          label='Check-out'
+          placeholder='Date'
+          className='rounded-l-none rounded-r-none'
+        />
+        <Button className='rounded-l-none w-full'>Search</Button>
       </div>
     </div>
   );
