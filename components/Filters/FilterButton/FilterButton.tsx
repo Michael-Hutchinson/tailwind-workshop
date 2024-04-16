2e6df4129706d2c750d99aadfa148773e58b1209
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-04-16 08:15:48 +0100

    feat: Add FilterButton component

---
+++ components/Filters/FilterButton/FilterButton.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils';
+
+interface FilterButtonProps {
+  label: string;
+  active?: boolean;
+}
+
+const FilterButton = ({ label, active }: FilterButtonProps) => {
+  return (
+    <button
+      className={cn(
+        active && 'bg-sky-500/40 text-neutral-800',
+        'p-2 rounded-xl text-muted-foreground'
+      )}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default FilterButton;
